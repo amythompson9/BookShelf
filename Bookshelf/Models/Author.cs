444c4f2c5001a67498f00f68c45dc211cbbbb64d
amythompson9 <amythompson9@gmail.com>
2017-10-11 20:47:17 -0400

    First Commit

---
+++ Bookshelf/Models/Author.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bookshelf.Models
+{
+    public class Author
+    {
+        [Key]
+        public string ID { get; set; }
+        public string LastFirst { get; set; }
+        public virtual ICollection<Author> Authors { get; set; }
+    }
+}
